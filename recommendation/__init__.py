e6964f034ce4f14c22b0e5cf9a4cf5d92a85f5bc
jammyjam-j <maeiteful2@gmail.com>
2026-03-05 17:52:02 +0100

    Add recommendation/__init__.py

---
+++ recommendation/__init__.py
@@ -0,0 +1,6 @@
+from .recommender import Recommender
+from .utils import preprocess_text, generate_embedding
+
+__version__ = "0.1.0"
+
+__all__ = ["Recommender", "preprocess_text", "generate_embedding"]
